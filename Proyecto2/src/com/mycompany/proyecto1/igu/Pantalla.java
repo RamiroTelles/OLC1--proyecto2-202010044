ba86531f79d6ac8025bc44e045808895e9596a34
RamiroTelles <78386364+RamiroTelles@users.noreply.github.com>
2024-01-04 04:38:58 -0600

    graficar AST bugfix

--- Proyecto2/src/com/mycompany/proyecto1/igu/Pantalla.java
+++ Proyecto2/src/com/mycompany/proyecto1/igu/Pantalla.java
@@ -1978,6 +1978,9 @@ public class Pantalla extends javax.swing.JFrame implements ActionListener{
         }else if(raiz.getLex().equals(":")){
             nodos += "n"+raiz.getId()+"[label=dosPuntos];\n";
             
+        }else if(raiz.getLex().equals("?")){
+            nodos += "n"+raiz.getId()+"[label=Interrogacion];\n";
+            
         }else if(raiz.getLex().charAt(0)=='\"'){
             //nodos += "n"+raiz.getId()+"[label="+raiz.getLex().substring(1,raiz.getLex().length()-1)+"];\n";
             nodos += "n"+raiz.getId()+"[label=cadena];\n";
